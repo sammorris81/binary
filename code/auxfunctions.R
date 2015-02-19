37242b4bc6a5ca60779ba48a0083c96592694173
sam-mba <sam.morris81@gmail.com>
2015-02-19 11:02:38 -0500

    working on mcmc to fit model

--- code/auxfunctions.R
+++ code/auxfunctions.R
@@ -1,31 +1,89 @@
-#### get z from covariates
-getZ <- function(xi, x.beta) {
-  z <- -(1 - xi * x.beta)^(-1 / xi)
-  return(z)
-}
-
-a2A <- function(FAC, a, alpha) {
+# theta sum_l=1^L a_l * w_l^(1 / alpha)
+getTheta <- function(w, a, alpha) {
   # theta is nxnF
   # s is nFxnt
   # alpha in (0,1)
-  W <- FAC^(1 / alpha)
-  if (length(a) == 1) {xxx <- W * a}
-  if (length(a) > 1) {xxx <- W %*% a}
+  w.star <- w^(1 / alpha)
+  if (length(a) == 1) {xxx <- w.star * a}
+  if (length(a) > 1) {xxx <- w.star %*% a}
   return(xxx)
 }
 
 # get the kernel weighting
-make.fac <- function(dw2, gamma) {
-  rho2 <- exp(gamma)^2
+makeW <- function(dw2, rho.star) {
+  # rho.star is in (-Inf, Inf)
+  rho2 <- exp(rho.star)^2
   fac <- exp(-0.5 * dw2 / rho2)
   return(fac)
 }
 
 # standardize the kernel weights
-fac2FAC <- function(x, single=F) {
+stdW <- function(x, single=FALSE) {
   if (single) {x <- x / sum(x)}
   if (!single) {x <- sweep(x, 1, rowSums(x), "/")}
   return(x)
+}
+
+# get find the ll for y - returns ns x nt matrix for each site/day
+logLikeY <- function(y, theta, alpha, z) {
+  z.star <- -(theta / z)^(1 / alpha)
+  ll.y <- (1 - y) * z.star + y * log(1 - z.star)
+  return(ll.y)
+}
+
+npts <- 50
+Ubeta <- qbeta(seq(0, 1, length=npts + 1), 0.5, 0.5)
+MidPoints <- (Ubeta[-1] + Ubeta[-(npts + 1)]) / 2
+BinWidth <- Ubeta[-1] - Ubeta[-(npts + 1)]
+
+dPS <- function(a, alpha, npts=100) {
+  l <- -Inf
+  if (a > 0) {
+    l <- log(sum(BinWidth * ld(MidPoints, a, alpha)))
+  }
+
+  return(l)
+}
+
+
+
+
+ECkern <- function(h, alpha, gamma, Lmax=50) {
+  dw2 <- rdist(c(0, h), seq(-Lmax, Lmax, 1))
+  W <- fac2FAC(make.fac(dw2, gamma))^(1 / alpha)
+  for (j in 1:length(h)) {
+    h[j]<-sum((W[1, ] + W[j + 1, ])^alpha)
+  }
+
+  return(h)
+}
+
+
+
+ld <- function(u, A, alpha) {
+  psi <- pi * u
+  c <- (sin(alpha * psi) / sin(psi))^(1 / (1 - alpha))
+  c <- c * sin((1 - alpha) * psi) / sin(alpha * psi)
+  logd <- log(alpha) - log(1 - alpha) - (1 / (1 - alpha)) * log(A) +
+          log(c) - c * (1 / A^(alpha / (1 - alpha)))
+
+  return(exp(logd))
+}
+
+
+ld2 <- function(u, logs, alpha, shift=0, log=T) {
+
+  logs <- logs - shift / alpha
+  s <- exp(logs)
+  psi <- pi * u
+  c <- (sin(alpha * psi) / sin(psi))^(1 / (1 - alpha))
+  c <- c * sin((1 - alpha) * psi) / sin(alpha * psi)
+
+  logd <- log(alpha) - log(1 - alpha) - (1 / (1 - alpha)) * logs +
+          log(c) - c * (1 / s^(alpha / (1 - alpha))) +
+          logs
+
+  return(logd)
 }
 
 
@@ -37,8 +95,12 @@ logd <- function(theta, v) {
 #### Used to set the standard deviation for the candidate distribution
 #### for the A terms in the random effect. When log(A) is large means
 #### sd is smaller, and log(A) small means sd is larger.
-get.level <- function(logs, cuts) {
-  sum(logs > cuts) + 1
+get.level <- function(a, cuts) {
+  lev <- a * 0 + 1
+  for (j in 1:length(cuts)) {
+    lev <- ifelse(a > cuts[j], j + 1, lev)
+  }
+  return(lev)
 }
 
 logdet <- function(X) {
--- code/updateBeta.R
+++ code/updateBeta.R
@@ -1,4 +1,6 @@
-updateBeta <- function(y, A, w, alpha, z, beta, x, cur.lly, acc, att, mh) {
+# update the beta term for the linear model
+updateBeta <- function(y, theta, alpha, z, beta, xi, x, cur.lly,
+                       acc, att, mh) {
   # at the moment coding a block update for all parameters
   p <- dim(x)[3]
   nt <- dim(x)[2]
@@ -14,13 +16,73 @@ updateBeta <- function(y, A, w, alpha, z, beta, x, cur.lly, acc, att, mh) {
   }
   can.z <- -(1 - xi * can.x.beta)^(-1 / xi)
 
-  can.lly <- logLikeY(y=y, A=A, w=w, alpha=alpha, z=can.z)
+  can.lly <- logLikeY(y=y, theta=theta, alpha=alpha, z=can.z)
+
+  R <- sum(can.lly - cur.lly) +
+       sum(dnorm(can.beta, log=TRUE)) - sum(dnorm(beta, log=TRUE))
+
   if (!is.na(R)) { if (log(runif(1)) < R) {
     beta    <- can.beta
     x.beta  <- can.x.beta
+    z       <- can.z
     cur.lly <- can.lly
     acc     <- acc + 1
   }}
 
-  results <- list(beta=beta, x.beta=x.beta, cur.lly=cur.lly, att, acc)
+  results <- list(beta=beta, x.beta=x.beta, z=z, cur.lly=cur.lly,
+                  att=att, acc=acc)
+}
+
+# update the xi term for the linear model
+updateXi <- function(y, theta, alpha, z, x.beta, xi, cur.lly,
+                     acc, att, mh) {
+  nt <- dim(x)[2]
+  att <- att + 1
+
+  can.xi <- rnorm(1, xi, mh)
+  can.z <- -(1 - can.xi * x.beta)^(-1 / can.xi)
+
+  can.lly <- logLikeY(y=y, theta=theta, alpha=alpha, z=can.z)
+
+  R <- sum(can.lly - cur.lly) +
+       dnorm(can.xi, log=TRUE) - dnorm(xi, log=TRUE)
+
+  if (!is.na(R)) { if (log(runif(1)) < R) {
+    xi      <- can.xi
+    z       <- can.z
+    cur.lly <- can.lly
+    acc     <- acc + 1
+  }}
+
+  results <- list(xi=xi, z=z, cur.lly=cur.lly, att=att, acc=acc)
+}
+
+# update the random effects for theta
+updateA <- function(y, theta, alpha, z, w, acc, att, mh) {
+
+  for (t in 1:nt) {
+    for (k in 1:nknots) {
+      l1        <- get.level(a[k, t], cuts)  # keeps sd reasonable
+      can.a     <- exp(rnorm(1, log(a[k, t]), mh[l1]))
+      l2        <- get.level(can.a, cuts)
+      www       <- w[, k]^(1 / alpha)
+      can.theta <- theta[, t] + www * (can.a - a[k, t])
+      can.lp    <- dPS(can.a, alpha)
+    }
+  }
+
+
+
+  results <- list(a=a, theta=theta, cur.lly=cur.lly, att=att, acc=acc)
+}
+
+# update the alpha term for theta
+updateAlpha <- function() {
+
+  results <- list(alpha=alpha, theta=theta, cur.lly=cur.lly, att=att, acc=acc)
+}
+
+updateRho <- function() {
+
+  results <- list(rho=rho, theta=theta, cur.lly=cur.lly, att=att, acc=acc)
 }
