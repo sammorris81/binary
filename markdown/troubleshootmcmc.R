fe775fa06865378aa9bd9428a04614780d27e21d
sam-ubuntu <samorris@ncsu.edu>
2015-06-04 15:42:40 -0400

    minor changes to markdown to remove cached images and datasets. Also adding in the troubleshooting file.

---
+++ markdown/troubleshootmcmc.R
@@ -0,0 +1,107 @@
+rm(list=ls())
+
+# libraries
+library(fields)
+library(Rcpp)
+library(evd)
+library(spBayes)
+library(mvtnorm)
+Sys.setenv("PKG_CXXFLAGS"="-fopenmp")
+Sys.setenv("PKG_LIBS"="-fopenmp")
+sourceCpp(file = "../code/R/pairwise.cpp")
+
+source("../code/R/auxfunctions.R", chdir = TRUE)
+source("../code/R/updateModel.R")
+source("../code/R/mcmc.R")
+source("../code/R/probit.R", chdir=T)
+
+# true knots
+knots.t <- as.matrix(expand.grid(seq(0.00, 1.00, length=12),
+                                 seq(0.00, 1.00, length=12)))
+
+# knots used to fit model
+knots <- as.matrix(expand.grid(seq(0.00, 1.00, length=15),
+                               seq(0.00, 1.00, length=15)))
+
+# grid for searching over alpha and rho
+rhos   <- (knots[2, 1] - knots[1, 1]) * seq(1, 3, by = 0.5)
+
+# get sites and distances from knots
+set.seed(7483)  # sites
+ns  <- 1500
+s   <- cbind(runif(ns), runif(ns))
+dw2 <- rdist(s, knots)
+d   <- as.matrix(rdist(s))
+diag(d) <- 0
+x   <- matrix(1, ns, 1)
+
+# only focusing on the case of strong dependence
+alpha.t <- 0.20
+xi.t    <- 0.25
+rho.t   <- 0.15
+prop    <- c(0.05, 0.01)
+
+# y is ns, nt, nsets, nsettings
+iters <- 40000; burn <- 30000; update <- 500; thin <- 1
+# iters <- 100; burn <- 50; update <- 10; thin <- 1
+# setup for spGLM
+n.report <- 500
+verbose <- TRUE
+tuning <- list("phi"=0.1, "sigma.sq"=0.1, "tau.sq"=0.1,
+               "beta"=0.1, "w"=0.1)
+starting <- list("phi"=3/0.5, "sigma.sq"=50, "tau.sq"=1,
+                 "beta"=0, "w"=0)
+priors <- list("beta.norm"=list(1, 100),
+               "phi.unif"=c(0.1, 1e4), "sigma.sq.ig"=c(1, 1),
+               "tau.sq.ig"=c(1, 1))
+cov.model <- "exponential"
+
+
+data.seed <- 3282
+set.seed(data.seed)  # data
+data <- rRareBinarySpat(x, s = s, knots = knots.t, beta = 0, xi = xi.t,
+                        alpha = alpha.t, rho = rho.t, prob.success = prop[1])
+y <- data$y
+
+ntrain <- floor(0.75 * ns)
+ntest  <- ns - ntrain
+obs <- c(rep(T, ntrain), rep(F, ntest))
+y.o <- y[obs, , drop = FALSE]
+X.o <- matrix(x[obs], ntrain, 1)
+s.o <- s[obs, ]
+y.validate <- y[!obs, , drop = FALSE]
+X.p <- matrix(x[!obs, ], ntest, 1)
+s.p <- s[!obs, ]
+
+nll <- rep(9999999, length(rhos))
+for (i in seq_along(rhos)) {
+  temp <- fit.rarebinaryCPP(c(0.5, 0, -4), rho = rhos[i],  y = y.o,
+                            dw2 = dw2, d = d, cov = X.o, threads = 6)
+  nll[i] <- temp$value
+  if (nll[i] == min(nll)) {
+    fit <- temp
+  }
+}
+
+fit <- fit.rarebinaryCPP(c(0.5, 0, -4), rho=rho.t, y = y.o, dw2 = dw2, d = d, cov=X.o, threads=6)
+rho.mcmc <- fit$rho
+
+
+xibeta.hat <- fit$par[2:3]
+xibeta.var <- solve(fit$hessian)[2:3, 2:3]
+
+# spatial GEV
+mcmc.seed <- 1
+set.seed(mcmc.seed)
+fit.gev <- mcmc(y = y.o, s = s.o, x = X.o, s.pred = NULL, x.pred = NULL,
+                beta.init = fit$par[3], beta.m = 0, beta.s = 100,
+                xi.init = fit$par[2], xi.m = 0, xi.s = 0.5,
+                knots = knots, beta.tune = 1, xi.tune = 0.1,
+                alpha.tune = 0.01, rho.tune = 0.1, A.tune = 1,
+                beta.attempts = 50, xi.attempts = 50,
+                alpha.attempts = 200, rho.attempts = 200,
+                spatial = TRUE, rho.init = fit$rho, rho.upper = 9,
+                alpha.init = fit$par[1], a.init = 100, iterplot = FALSE,
+                alpha.fix = TRUE, rho.fix = TRUE, xibeta.joint = TRUE,
+                xibeta.hat = xibeta.hat, xibeta.var = xibeta.var,
+                iters = iters, burn = burn, update = update, thin = 1)
