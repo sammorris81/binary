3f09aadb8fb6bacba7e2c9b9cc95cc39b3079c30
sam-lab <samorris@ncsu.edu>
2015-08-08 23:03:59 -0400

    multiple computers

--- markdown/sim-hotspot-1/hotspot-sim4.R
+++ markdown/sim-hotspot-1/hotspot-sim4.R
@@ -23,6 +23,7 @@ load("./simdata.RData")
 
 # data setting and sets to include - written by bash script
 settings <- c(1, 2, 3, 4)
+sets <- c(100)
 
 for (setting in 1:length(settings)) {
   # extract the relevant setting from simdata
