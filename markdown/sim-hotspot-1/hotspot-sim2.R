8aa3e992eaf85dd30a8acd68b8562ba314c156ab
sam-imac <sam.morris81@gmail.com>
2015-08-08 22:12:34 -0500

    for run on imac

--- markdown/sim-hotspot-1/hotspot-sim2.R
+++ markdown/sim-hotspot-1/hotspot-sim2.R
@@ -23,7 +23,7 @@ load("./simdata.RData")
 
 # data setting and sets to include - written by bash script
 settings <- c(1, 2, 3, 4)
-sets <- c(4, 5, 6)
+sets <- c(19, 20, 21)
 
 for (setting in 1:length(settings)) {
   # extract the relevant setting from simdata
--- markdown/sim-hotspot-1/hotspot-sim3.R
+++ markdown/sim-hotspot-1/hotspot-sim3.R
@@ -23,7 +23,7 @@ load("./simdata.RData")
 
 # data setting and sets to include - written by bash script
 settings <- c(1, 2, 3, 4)
-sets <- c(7, 8, 9)
+sets <- c(22, 23, 24)
 
 for (setting in 1:length(settings)) {
   # extract the relevant setting from simdata
