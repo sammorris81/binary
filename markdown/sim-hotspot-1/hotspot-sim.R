1225c5f391e64c8bc40a5401a06c05424b8f106e
Sam Morris <sam.morris81@gmail.com>
2015-08-09 11:17:08 -0400

    comment out logit fit

--- markdown/sim-hotspot-1/hotspot-sim.R
+++ markdown/sim-hotspot-1/hotspot-sim.R
@@ -175,41 +175,41 @@ for (i in sets) {
   # upload.cmd <- paste("scp ", tblname, " samorris@hpc.stat.ncsu.edu:~/rare-binary/markdown/sim-hotspot-1/sim-tables", sep = "")
   # system(upload.cmd)
   
-  # spatial logit
-  print("  start logit")
-  print("    start mcmc fit")
-  mcmc.seed <- mcmc.seed + 1
-  set.seed(mcmc.seed)
-  tic       <- proc.time()[3]
-  fit.logit <- spGLM(formula = y.i.o ~ 1, family = "binomial",
-                     coords = s.o, knots = knots, starting = starting,
-                     tuning = tuning, priors = priors,
-                     cov.model = cov.model, n.samples = iters,
-                     verbose = verbose, n.report = n.report, amcmc = amcmc)
-  
-  print("    start mcmc predict")
-  yp.sp.log <- spPredict(sp.obj = fit.logit, pred.coords = s.p,
-                         pred.covars = X.p, start = burn + 1,
-                         end = iters, thin = 1, verbose = TRUE,
-                         n.report = 500)
-  
-  post.prob.log <- t(yp.sp.log$p.y.predictive.samples)
-  toc        <- proc.time()[3]
-  timings[3] <- toc - tic
-  
-  bs.log <- BrierScore(post.prob.log, y.i.p)
-  print(bs.log * 100)
+#   # spatial logit
+#   print("  start logit")
+#   print("    start mcmc fit")
+#   mcmc.seed <- mcmc.seed + 1
+#   set.seed(mcmc.seed)
+#   tic       <- proc.time()[3]
+#   fit.logit <- spGLM(formula = y.i.o ~ 1, family = "binomial",
+#                      coords = s.o, knots = knots, starting = starting,
+#                      tuning = tuning, priors = priors,
+#                      cov.model = cov.model, n.samples = iters,
+#                      verbose = verbose, n.report = n.report, amcmc = amcmc)
+#   
+#   print("    start mcmc predict")
+#   yp.sp.log <- spPredict(sp.obj = fit.logit, pred.coords = s.p,
+#                          pred.covars = X.p, start = burn + 1,
+#                          end = iters, thin = 1, verbose = TRUE,
+#                          n.report = 500)
+#   
+#   post.prob.log <- t(yp.sp.log$p.y.predictive.samples)
+#   toc        <- proc.time()[3]
+#   timings[3] <- toc - tic
+#   
+#   bs.log <- BrierScore(post.prob.log, y.i.p)
+#   print(bs.log * 100)
 
   print(paste("Finished: Set ", i, sep = ""))
   save(fit.pcl, fit.gev, post.prob.gev, bs.gev,
        fit.probit, post.prob.pro, bs.pro, 
-       fit.logit, post.prob.log, bs.log,
+       # fit.logit, post.prob.log, bs.log,
        y.i.p, y.i.o, s, timings,
        file = filename)
   
   # copy table to tables folder on beowulf
-  bs <- rbind(bs.gev, bs.pro, bs.log)
-  write.table(bs, file = tblname)
+  # bs <- rbind(bs.gev, bs.pro, bs.log)
+  # write.table(bs, file = tblname)
   # upload.cmd <- paste("scp ", tblname, " samorris@hpc.stat.ncsu.edu:~/rare-binary/markdown/sim-hotspot-1/sim-tables", sep = "")
   # system(upload.cmd)
 }
