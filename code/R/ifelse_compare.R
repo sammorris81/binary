d3e9affafb03d37277da783a7e5c7a194d79ea4e
Sam Morris <sam.morris81@gmail.com>
2015-09-20 15:14:18 -0500

    trying different compiler flags

--- code/R/ifelse_compare.R
+++ code/R/ifelse_compare.R
@@ -4,7 +4,9 @@ library(microbenchmark)
 library(Rcpp)
 Sys.setenv("PKG_CXXFLAGS"="-fopenmp")
 Sys.setenv("PKG_LIBS"="-fopenmp")
-sourceCpp(file = "./ifelse.cpp")
+Sys.setenv("PKG_CXXFLAGS"="-march=native -fopenmp")  # what if we change the march option
+Sys.setenv("PKG_LIBS"="-march=native -fopenmp")
+sourceCpp(file = "./ifelse.cpp", rebuild = TRUE)
 
 set.seed(2087)
 temp <- matrix(rnorm(10000), 100, 100)
@@ -29,3 +31,4 @@ temp <- ifelsevecCPP(temp, 2)
 mean(temp)
 
 microbenchmark(ifelse(abs(temp) < 2, 0, temp), ifelsematCPP(temp, 2))
+
