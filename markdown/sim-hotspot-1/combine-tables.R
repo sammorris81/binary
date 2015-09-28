901d1f3afab492ed355c3320bdf35701727bc223
Sam Morris <sam.morris81@gmail.com>
2015-09-28 01:09:47 -0500

    updates to results

--- markdown/sim-hotspot-1/combine-tables.R
+++ markdown/sim-hotspot-1/combine-tables.R
@@ -24,9 +24,12 @@ for (i in 1:length(files)) {
 }
 
 results.combined <- matrix(NA, nsettings, nmethods)
+results.sd       <- matrix(NA, nsettings, nmethods)
 for (setting in 1:nsettings) {
   results.combined[setting, ] <- apply(results[[setting]], 2, mean, 
                                        na.rm = TRUE)
+  results.sd[setting, ] <- apply(results[[setting]], 2, sd, 
+                                 na.rm = TRUE)
 }
 
 # how many have finished
@@ -39,3 +42,35 @@ setting.1 <- which(rowSums(is.na(results[[1]])) == 3)
 setting.2 <- which(rowSums(is.na(results[[2]])) == 3)
 setting.3 <- which(rowSums(is.na(results[[3]])) == 3)
 setting.4 <- which(rowSums(is.na(results[[4]])) == 3)
+
+# Check for differences
+# First do Friedman test (one-way repeated measures)
+#   friedman.test(y ~ trt | block, data)
+# Then follow up with the Wilcoxon, Nemenyi, McDonald-Thompson test
+# pWNMT(x, b, trt, method, n.mc)
+#     x: list of values
+#     b: vector of blocks (only needed if x is a vector)
+#     trt: vector of treatments
+#     method: "Exact", "Monte Carlo" or "Asymptotic"
+
+groups <- rep(1:(nmethods - 1), each=100)
+dataset <- rep(1:100, times=(nmethods - 1))
+
+library(NSM3)
+results.friedman <- rep(0, nsettings)
+for (setting in 1:nsettings) {
+  scores <- as.vector(results[[setting]][, c(1, 2)])
+  combine <- data.frame(scores, groups, dataset)
+  results.friedman[setting] <- friedman.test(scores ~ groups | dataset,
+                                             data=combine)$p.value
+}
+
+# posthoc is  Wilcoxon, Nemenyi, McDonald-Thompson test
+results.wnmt <- matrix(0, nrow = choose((nmethods - 1), 2), ncol = nsettings)
+for (setting in 1:nsettings) {
+  scores <- as.vector(results[[setting]][, c(1, 2)])
+  combine <- data.frame(scores, groups, dataset)
+  results.wnmt[, setting] <- pWNMT(x=combine$scores, b=combine$dataset,
+                                   trt=combine$groups, n.mc=20000)$p.val
+  print(paste("setting:", setting))
+}
