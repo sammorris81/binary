d0ec049b93c730d5bc6cc08475c405d547a9f5f0
Sam Morris <sam.morris81@gmail.com>
2015-11-05 07:53:44 -0600

    rename HMC.R

---
+++ code/R/gevHMC.R
@@ -0,0 +1,208 @@
+# This is slighty modified.  I added the "others" input and 
+#  changed the format of the output
+#
+# SIMPLE IMPLEMENTATION OF HAMILTONIAN MONTE CARLO.
+#
+# Radford M. Neal, 2010.
+#
+# This program appears in Figure 2 of "MCMC using Hamiltonian dynamics",
+# to appear in the Handbook of Markov Chain Monte Carlo.
+#
+# The arguments to the HMC function are as follows:
+#
+#   U          A function to evaluate minus the log of the density of the
+#              distribution to be sampled, plus any constant - ie, the
+#              "potential energy".
+#
+#   grad_U     A function to evaluate the gradient of U.
+#
+#   epsilon    The stepsize to use for the leapfrog steps.
+#
+#   L          The number of leapfrog steps to do to propose a new state.
+#
+#   current_q  The current state (position variables only).
+#
+# Momentum variables are sampled from independent standard normal
+# distributions within this function.  The value return is the vector
+# of new position variables (equal to current_q if the endpoint of the
+# trajectory was rejected).
+#
+# This function was written for illustrative purposes.  More elaborate
+# implementations of this basic HMC method and various variants of HMC
+# are available from my web page, http://www.cs.utoronto.ca/~radford/
+
+# ------------------------------------------------------------------
+# This version of the function returns (as attributes of the result)
+# information for assessing how well the No U-Turn sampler might work.
+# The the distance of each position along the trajectory from the
+# start position is returned as "dist", and with whether the
+# trajectory end-point was accepted is returned as "accept".
+# ------------------------------------------------------------------
+
+
+gevHMC = function (U, grad_U, current_q, epsilon = 0.01, L = 10, 
+                data, beta, xi, a, b, alpha, rho, calc, others, this.param)
+{
+  dist = numeric(L+1)
+  
+  if (this.param == "a_alpha") {
+    infinite <- c(FALSE, FALSE)
+  } else {
+    infinite <- FALSE
+  }
+  
+  q = current_q
+  p = rnorm(length(q), 0, 1)  # independent standard normal variates
+  current_p = p
+
+  # Make a half step for momentum at the beginning
+
+  p = p - epsilon * grad_U(q = q, data = data, beta = beta, xi = xi, a = a, 
+                           b = b, alpha = alpha, rho = rho, calc = calc,
+                           others = others) / 2
+
+  # Alternate full steps for position and momentum
+
+  for (i in 1:L)
+  { 
+    # Make a full step for the position
+    # if (any(is.nan(p))) {
+#       p.trouble.hmc <<- p
+#       q.trouble.hmc <<- q
+#       beta.trouble.hmc <<- beta
+#       xi.trouble.hmc <<- xi
+#       a.trouble.hmc <<- a
+#       b.trouble.hmc <<- b
+#       alpha.trouble.hmc <<- alpha
+#       rho.trouble.hmc <<- rho
+#       calc.trouble.hmc <<- calc
+#       stop("p is NaN in HMC")
+    # }
+
+    q = q + epsilon * p
+    if (any(q == Inf | q == -Inf)) {
+      print(paste("Proposed variable is infinity for", this.param, "Automatically rejecting."))
+      if (this.param == "a_alpha") {
+        infinite = c(FALSE, FALSE)
+        if(any(q[1:(length(q) - 1)] == Inf | q[1:(length(q) - 1)] == -Inf)) {
+          print("inf for a in spot")
+          print(which(q[1:(length(q) - 1)] == Inf | q[1:(length(q) - 1)] == -Inf))
+          infinite[1] <- TRUE
+        }
+        if (tail(q, 1) == Inf | tail(q, 1) == -Inf) {
+          print("inf for alpha")
+          infinite[2] <- TRUE
+        }
+        return(list(q = current_q, accept = FALSE, infinite = infinite))
+      } else {
+        return(list(q = current_q, accept = FALSE, infinite = TRUE))
+      }
+      
+    }
+    
+    dist[i+1] = sqrt(sum((q - current_q)^2))
+
+    # Make a full step for the momentum, except at end of trajectory
+
+    if (i!=L) p = p - epsilon * grad_U(q = q, data = data, beta = beta, xi = xi,
+                                       a = a, b = b, alpha = alpha, rho = rho,
+                                       calc = calc, others = others)
+    if (any(is.nan(p))) {
+      print(paste("NaN in p for", this.param, "automatically rejecting"))
+      if (this.param == "a_alpha") {
+        infinite = c(FALSE, FALSE)
+        if (any(is.nan(p[1:(length(p) - 1)]))) {
+          print("nan for a in spot")
+          print(which(is.nan(p[1:(length(p) - 1)])))
+          infinite[1] <- TRUE
+        }
+        if (tail(is.nan(p), 1)) {
+          print("nan for alpha")
+          infinite[2] <- TRUE
+        }
+        return(list(q = current_q, accept = FALSE, infinite = infinite))
+      } else {
+        return(list(q = current_q, accept = FALSE, infinite = TRUE))
+      }
+    } else if (any(p == Inf | p == -Inf)) {
+      print(paste("Momentum variable is infinity for", this.param, "Automatically rejecting."))
+      return(list(q = current_q, accept = FALSE, infinite = TRUE))
+    }
+#     if (any(is.nan(p))) print(i)
+#     if (any(is.nan(q))) print(i)
+  }
+
+  # Make a half step for momentum at the end.
+
+  p = p - epsilon * grad_U(q = q, data = data, beta = beta, xi = xi, a = a, 
+                           b = b, alpha = alpha, rho = rho, calc = calc,
+                           other = others) / 2
+
+  # Negate momentum at end of trajectory to make the proposal symmetric
+
+  p = -p
+
+  # Evaluate potential & kinetic energies at start & end of trajectory
+  
+  current_U = U(current_q, data = data, beta = beta, xi = xi, a = a, b = b, 
+                alpha = alpha, rho = rho, calc = calc, others = others)
+  current_K = sum(current_p^2) / 2
+  proposed_U = U(q, data = data, beta = beta, xi = xi, a = a, b = b, 
+                 alpha = alpha, rho = rho, calc = calc, others = others)
+  proposed_K = sum(p^2) / 2
+  
+#   if (any(is.nan(current_U))) {
+#     print("The value of the log likelihood is NaN at the current values")
+#     print(paste("Parameter: ", this.param, "=", current_q))
+#     print(paste("epsilon is: ", epsilon))
+#   } else if (any(is.nan(current_K))) {
+#     print("The potential is NaN at at the current values")
+#     print(paste("Parameter: ", this.param))
+#     print(paste("epsilon is: ", epsilon))
+#   } else if (any(is.nan(proposed_U))) {
+#     print("The value of the log likelihood is NaN at the proposed values")
+#     print(paste("Parameter: ", this.param, "=", q))
+#     print(paste("epsilon is: ", epsilon))
+#     q.trouble <<- q
+#     beta.trouble <<- beta
+#     xi.trouble <<- xi
+#     a.trouble <<- a
+#     b.trouble <<- b
+#     alpha.trouble <<- alpha
+#     rho.trouble <<- rho
+#     calc.trouble <<- calc
+#   } else if (any(is.nan(proposed_K))) {
+#     print("The potential is NaN at the proposed values")
+#     print(paste("Parameter: ", this.param))
+#     print(paste("epsilon is: ", epsilon))
+#   }
+  
+  R <- current_U - proposed_U + current_K - proposed_K
+  # print(paste("R is ", R))
+
+  # Accept or reject the state at end of trajectory, returning either
+  # the position at the end of the trajectory or the initial position
+  if (!is.nan(R)) {
+    if (runif(1) < exp(R))
+    {
+      return(list(q = q, accept = TRUE, infinite = infinite))
+    }
+    else
+    {
+      return(list(q = current_q, accept = FALSE, infinite = infinite))
+    }
+  } else {
+    if (this.param == "a_alpha") {
+      infinite = c(FALSE, FALSE)
+      if (tail(is.nan(p), 1)) {
+        infinite[2] <- TRUE
+      }
+      if (any(is.nan(p[1:(length(p) - 1)]))) {
+        infinite[1] <- TRUE
+      }
+      return(list(q = current_q, accept = FALSE, infinite = infinite))
+    } else {
+      return(list(q = current_q, accept = FALSE, infinite = infinite))
+    }
+  }
+}
