8e80b2e422fb094a5ab9ce3293d7c60244255c59
Sam Morris <sam.morris81@gmail.com>
2015-11-10 23:47:17 -0600

    manuscript updates

--- markdown/sim-hmc/predict-maps.R
+++ markdown/sim-hmc/predict-maps.R
@@ -21,16 +21,6 @@ source("../../code/R/spatial_probit.R", chdir = TRUE)
 # get the datasets
 load("./simdata.RData")
 
-# extract the relevant setting from simdata
-y <- simdata[[setting]]$y
-s <- simdata[[setting]]$s
-x <- simdata[[setting]]$x
-
-# some precalculated values for quicker pairwise evaluation
-dw2     <- rdist(s, knots)
-d       <- rdist(s)
-diag(d) <- 0
-
 # sites at which to make predictions
 s.p    <- expand.grid(seq(0, 1, length = 51), seq(0, 1, length = 51))
 X.p    <- matrix(1, nrow(s.p), 1)
@@ -41,26 +31,27 @@ settings <- c(1:4)
 gev.map <- array(0, dim=c(iters - burn, nrow(s.p), length(settings)))
 pro.map <- array(0, dim=c(iters - burn, nrow(s.p), length(settings)))
 log.map <- array(0, dim=c(iters - burn, nrow(s.p), length(settings)))
+set <- rep(0, 4)
 for (setting in settings) {
-  set <- which(results[[setting]][, 1] - results[[setting]][, 2] == 
+  set[setting] <- which(results[[setting]][, 1] - results[[setting]][, 2] == 
                  min(results[[setting]][, 1] - results[[setting]][, 2], na.rm = TRUE))
-  filename <- paste("sim-results/", setting, "-", set, ".RData", sep = "")
-  load(filename)
-  
-  cat("Start gev \n")
-  gev.map[, , setting] <- pred.spgev(mcmcoutput = fit.gev, x.pred = X.p,
-                                     s.pred = s.p, knots = knots, start = 1, 
-                                     end = iters - burn, update = update)
-  
-  cat("Start probit \n")
-  pro.map[, , setting] <- pred.spprob(mcmcoutput = fit.probit, X.pred = X.p, 
-                                      s.pred = s.p, knots = knots, start = 1, 
-                                      end = iters - burn, update = update)
-  
-  cat("Start logit \n")
-  log.map[, , setting] <- pred.splogit(mcmcoutput = fit.logit, 
-                                       s.pred = s.p, knots = knots, start = 1, 
-                                       end = iters - burn, update = update)
+#   filename <- paste("sim-results/", setting, "-", set, ".RData", sep = "")
+#   load(filename)
+#   
+#   cat("Start gev \n")
+#   gev.map[, , setting] <- pred.spgev(mcmcoutput = fit.gev, x.pred = X.p,
+#                                      s.pred = s.p, knots = knots, start = 1, 
+#                                      end = iters - burn, update = update)
+#   
+#   cat("Start probit \n")
+#   pro.map[, , setting] <- pred.spprob(mcmcoutput = fit.probit, X.pred = X.p, 
+#                                       s.pred = s.p, knots = knots, start = 1, 
+#                                       end = iters - burn, update = update)
+#   
+#   cat("Start logit \n")
+#   log.map[, , setting] <- pred.splogit(mcmcoutput = fit.logit, 
+#                                        s.pred = s.p, knots = knots, start = 1, 
+#                                        end = iters - burn, update = update)
 }
 
 gev.summary <- array(0, dim = c(nrow(s.p), 5, length(settings)))
@@ -87,15 +78,27 @@ for (setting in settings) {
   log.summary[, 5, setting] <- apply(log.map[, , setting], 2, sd)
 }
 
-save(s.p, gev.summary, pro.summary, log.summary, file = "maps.RData")
+save(s.p, sets, settings, gev.summary, pro.summary, log.summary, file = "maps.RData")
+load("maps.RData")
+load("./simdata.RData")
 
 # generate plots of posterior summaries
 for (setting in settings) {
+  # extract the relevant setting from simdata
+  y <- simdata[[setting]]$y
+  s <- simdata[[setting]]$s
+  x <- simdata[[setting]]$x
+  set <- sets[setting]
+  
   dev.new(width = 7, height = 7)
   par(mfrow = c(2, 2))
   quilt.plot(x = s.p[, 1], y = s.p[, 2], z = gev.summary[, 3, setting], nrow = 51, ncol = 51, main = "GEV")
   quilt.plot(x = s.p[, 1], y = s.p[, 2], z = pro.summary[, 3, setting], nrow = 51, ncol = 51, main = "Probit")
   quilt.plot(x = s.p[, 1], y = s.p[, 2], z = log.summary[, 3, setting], nrow = 51, ncol = 51, main = "Logit")
+  plot(knots, ylim = c(0, 1), xlim = c(0, 1), 
+       main = "Simulated dataset", xlab = "", ylab = "")
+  points(s[which(y[, set] != 1), ], pch = 21, col = "dodgerblue4", bg = "dodgerblue1")
+  points(s[which(y[, set] == 1), ], pch = 21, col = "firebrick4", bg = "firebrick1")
   filename <- paste("plots/post-med-", setting, ".pdf", sep = "")
   dev.print(device = pdf, file = filename)
   dev.off
