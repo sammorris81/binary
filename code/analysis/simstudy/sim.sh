21a73f4e66ffe7e57a3ce1e086e8f77c37428e22
Sam Morris <sam.morris81@gmail.com>
2016-07-04 15:39:06 -0600

    changing print statement

--- code/analysis/simstudy/sim.sh
+++ code/analysis/simstudy/sim.sh
@@ -5,7 +5,7 @@ SIM=$HOME/repos-git/rare-binary/code/analysis/simstudy
 L=$1
 COPY=$2
 
-echo "$COPY"
+echo "Setting: $L, Copy: $COPY"
 $RBIN CMD BATCH --vanilla --no-save $SIM/sim-$L.R $SIM/sim-$L-$COPY.out 2>&1
 
 exit 0
