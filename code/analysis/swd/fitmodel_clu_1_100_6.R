6dbc16aa66970c6cad7cbec70115bb9279114b30
Sam Morris <sam.morris81@gmail.com>
2016-07-23 17:42:20 -0500

    running additional datasets for n = 100 on species 1

---
+++ code/analysis/swd/fitmodel_clu_1_100_6.R
@@ -0,0 +1,7 @@
+source(file = "./package_load.R", chdir = T)
+cluster <- TRUE
+n <- 100
+these.sets <- 51:60
+which.y <- 1
+
+source("fitmodel_swd.R", chdir = TRUE)
---
+++ code/analysis/swd/fitmodel_srs_1_100_7.R
@@ -0,0 +1,7 @@
+source(file = "./package_load.R", chdir = T)
+cluster <- FALSE
+n <- 100
+these.sets <- 61:70
+which.y <- 1
+
+source("fitmodel_swd.R", chdir = TRUE)
