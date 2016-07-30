e28bf84d3da2776c11b3ceed67bb0b2e32e2c001
Sam Morris <sam.morris81@gmail.com>
2016-07-29 22:58:10 -0500

    accidentally deleted and stopped job on UWL imac

--- code/analysis/swd/fitmodel_srs_1_4.R
+++ code/analysis/swd/fitmodel_srs_1_4.R
@@ -1,15 +1,15 @@
-source(file = "./package_load.R", chdir = T)
-cluster <- FALSE
-n <- 100
-these.sets <- 31:40
-which.y <- 1
-
-source("fitmodel_swd.R", chdir = TRUE)
+# source(file = "./package_load.R", chdir = T)
+# cluster <- FALSE
+# n <- 100
+# these.sets <- 31:40
+# which.y <- 1
+# 
+# source("fitmodel_swd.R", chdir = TRUE)
 
 source(file = "./package_load.R", chdir = T)
 cluster <- FALSE
 n <- 250
-these.sets <- 31:40
+these.sets <- 38:40
 which.y <- 1
 
 source("fitmodel_swd.R", chdir = TRUE)
