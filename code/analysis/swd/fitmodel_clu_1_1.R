f819298b69cdd2a5a4db4f482d2adf3d1eba758c
Sam Morris <sam.morris81@gmail.com>
2017-06-25 19:36:06 -0700

    Turn back to actual analsyis, not timing.

--- code/analysis/swd/fitmodel_clu_1_1.R
+++ code/analysis/swd/fitmodel_clu_1_1.R
@@ -4,4 +4,12 @@ n <- 100
 these.sets <- 1:10
 which.y <- 1
 
-source("fitmodel_swd_time.R", chdir = TRUE)
+source("fitmodel_swd.R", chdir = TRUE)
+
+source(file = "./package_load.R", chdir = T)
+cluster <- TRUE
+n <- 250
+these.sets <- 1:10
+which.y <- 1
+
+source("fitmodel_swd.R", chdir = TRUE)
